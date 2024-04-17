3fbe285d571a4322ea789a3d9dd8a64dcc716462
Andrii Stepanenko <andrii.stp@gmail.com>
2024-04-17 13:01:07 +0300

    zdc

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import "./App.css";
 
 function App() {
@@ -9,3 +10,60 @@ function App() {
 }
 
 export default App;
+
+// class Counter extends React.Component {
+//   static defaultProps = {
+//     initialValue: 0,
+//   };
+
+//   state = { value: this.props.initialValue };
+
+//   handleInc = () => {
+//     this.setState((prevState) => ({
+//       value: prevState.value + 1,
+//     }));
+//   };
+
+//   handleIDec = () => {
+//     this.setState((prevState) => ({
+//       value: prevState.value - 1,
+//     }));
+//   };
+
+//   render() {
+//     return (\
+//     )
+//   }
+// }
+
+class Dropdown extends Component {
+  state = {
+    visible: false,
+  };
+
+  show = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  hide = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className="Dropdown">
+        <button type="button" className="Dropdown-toggle" onClick={this.show}>
+          Show
+        </button>
+        <button type="button" className="Dropdown-toggle" onClick={this.hide}>
+          HIDE
+        </button>
+        <div>MENU</div>
+      </div>
+    );
+  }
+}
