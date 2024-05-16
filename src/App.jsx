4c5647b2e11f5002757dcf7bb0e3c61a4f211cc0
Andrii Stepanenko <andrii.stp@gmail.com>
2024-05-16 11:36:09 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,63 @@
-import Feedback from "./components/Feedback";
+import { Component } from "react";
+
+import Statistics from "./components/Statistics/Statistics";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <Feedback />
-    </>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = (option) => {
+    this.setState((prevState) => ({ [option]: prevState[option] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    if (totalFeedback === 0) {
+      return 0;
+    }
+    return Math.round((good / totalFeedback) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <div>
+        <h1>Please leave Feedback</h1>
+        <button type="button" onClick={() => this.handleClick("good")}>
+          Good
+        </button>
+        <button type="button" onClick={() => this.handleClick("neutral")}>
+          Neutral
+        </button>
+        <button type="button" onClick={() => this.handleClick("bad")}>
+          Bad
+        </button>
+        {totalFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            totalFeedback={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <p>No feedback</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
